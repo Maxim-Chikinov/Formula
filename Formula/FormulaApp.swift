fa1f164fe3e99bb31a744d5eba37ef732cef3d50
Chikinov Maxim <maxim.tchikinov@yandex.ru>
2024-05-31 22:40:45 +0300

    Initial Commit

---
+++ Formula/FormulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormulaApp.swift
+//  Formula
+//
+//  Created by Chikinov Maxim on 31.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct FormulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
